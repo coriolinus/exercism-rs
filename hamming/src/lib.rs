3a376c5ddea8cda1cc760f9c705da44a3aad1a4c
Peter Goodspeed-Niklaus <peter.r.goodspeedniklaus@gmail.com>
2016-07-05 16:19:39 +0200

    Use match instead of if. Don't keep/count 0 values (speed!)

--- hamming/src/lib.rs
+++ hamming/src/lib.rs
@@ -1,11 +1,10 @@
 pub fn hamming_distance(a: &str, b: &str) -> Result<usize, &'static str> {
-    if a.len() != b.len() {
-        return Err("inputs of different length");
+    match a.len() == b.len() {
+        false => Err("inputs of different length"),
+        true => Ok(
+            a.chars().zip(b.chars())
+                .filter(|&(ac, bc)| ac != bc)
+                .count()
+        )
     }
-
-    Ok(
-        a.chars().zip(b.chars())
-            .map(|(ac, bc)| if ac == bc { 0 } else { 1 })
-            .fold(0, std::ops::Add::add)
-    )
 }
