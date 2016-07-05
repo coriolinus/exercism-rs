9bfc58f8a4ed9d7a442209c8b1a51005164c22be
Peter Goodspeed-Niklaus <peter.r.goodspeedniklaus@gmail.com>
2016-07-05 22:11:35 +0200

    All tests pass

--- word-count/src/lib.rs
+++ word-count/src/lib.rs
@@ -1,9 +1,13 @@
 use std::collections::HashMap;
 
+fn allowed_char(c: &char) -> bool {
+    c.is_alphabetic() || c.is_numeric()
+}
+
 pub fn word_count(s: &str) -> HashMap<String, u32> {
     let mut ret = HashMap::new();
-    for word in s.split(" ") {
-        let word: String = word.chars().filter(|c| c.is_alphabetic()).collect();
+    for word in s.to_lowercase().split(|c: char| !(allowed_char(&c))) {
+        let word: String = word.chars().filter(allowed_char).collect();
         if word.len() > 0 {
             let counter = ret.entry(word).or_insert(0);
             *counter += 1;
