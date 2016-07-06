308131dd36aa910ea6181a58150f6a6182bc47f3
Peter Goodspeed-Niklaus <peter.r.goodspeedniklaus@gmail.com>
2016-07-06 13:54:27 +0200

    Generator-based sieve of eratosthenes

--- sieve/src/lib.rs
+++ sieve/src/lib.rs
@@ -1,19 +1,45 @@
 /// Sieve of Eratosthenes
-/// classical implementation
-pub fn primes_up_to(limit: u32) -> Vec<u32> {
-    let mut primes = Vec::new();
-    let mut known_nonprime = vec![false; (limit + 1) as usize];
+/// generator implementation
+pub fn primes_up_to(limit: u64) -> Vec<u64> {
+    PrimesIter::new().take_while(|p| p <= &limit).collect()
+}
 
-    for i in 2..limit + 1 {
-        if !known_nonprime[i as usize] {
-            primes.push(i);
-            let mut counter = 2;
-            while counter * i <= limit {
-                known_nonprime[(counter * i) as usize] = true;
-                counter += 1;
-            }
+pub struct PrimesIter {
+    counter: u64,
+    primes: Vec<u64>,
+}
+
+impl PrimesIter {
+    pub fn new() -> Self {
+        PrimesIter {
+            counter: 2,
+            primes: Vec::new(),
         }
     }
 
-    primes
+    /// Test if a given number is prime, given the current state.
+    /// Not general purpose! Depends on having a complete list of all primes up to sqrt(n).
+    fn is_prime(&self, n: u64) -> bool {
+        for prime in self.primes.iter().take_while(|&p| p * p <= n) {
+            if n % prime == 0 {
+                return false;
+            }
+        }
+        true
+    }
+}
+
+impl Iterator for PrimesIter {
+    type Item = u64;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let mut counter = self.counter;
+        while ! self.is_prime(counter) {
+            counter += 1;
+        }
+        self.primes.push(counter);
+
+        self.counter = counter + 1;
+        Some(counter)
+    }
 }
