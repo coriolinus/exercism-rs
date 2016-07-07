0ed465f7a4bed4f8124ac39ad5c149beba2f7277
Peter Goodspeed-Niklaus <peter.r.goodspeedniklaus@gmail.com>
2016-07-07 17:40:35 +0200

    Fix a dumb error leaving out constants

--- hexadecimal/src/lib.rs
+++ hexadecimal/src/lib.rs
@@ -2,10 +2,10 @@ pub fn hex_to_int(hex: &str) -> Option<usize> {
     let mut result = 0;
     let mut position = 0;
     for ch in hex.chars().rev() {
-        let value: usize = match ch {
+        let value = match ch {
             '0'...'9' => ch as u8 - '0' as u8, // produces 0-9
-            'A'...'F' => ch as u8 - 'A' as u8, // produces 10-16
-            'a'...'f' => ch as u8 - 'a' as u8, // produces 10-16
+            'A'...'F' => ch as u8 - 'A' as u8 + 10, // produces 10-16
+            'a'...'f' => ch as u8 - 'a' as u8 + 10, // produces 10-16
             _ => return None
         } as usize;
         result += value  * (16_usize.pow(position));
