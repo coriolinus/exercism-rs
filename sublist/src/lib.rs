643518f5811ade3284851eaabea42955d4c167ed
Peter Goodspeed-Niklaus <peter.r.goodspeedniklaus@gmail.com>
2016-07-12 09:40:46 +0200

    All tests pass, quickly

--- sublist/src/lib.rs
+++ sublist/src/lib.rs
@@ -16,7 +16,7 @@ impl Comparison {
     }
 }
 
-pub fn sublist<T: Eq>(a: &[T], b: &[T]) -> Comparison {
+pub fn sublist<T: PartialEq>(a: &[T], b: &[T]) -> Comparison {
     if a.len() > b.len() {
         sublist(b, a).invert()
     } else if a.len() == b.len() {
@@ -25,27 +25,70 @@ pub fn sublist<T: Eq>(a: &[T], b: &[T]) -> Comparison {
         } else {
             Comparison::Unequal
         }
+    } else if a.len() == 0 {
+        Comparison::Sublist
     } else {
-        // a is a list shorter than b with at least 1 element.
+        // a is a list shorter than b
         // Is it a sublist of b?
-        // I dunno, let's try recursive descent to find out.
-        // That is, let's try reducing the length of B from either side until
-        // we get to the length of A, at which point we'll know.
-        let drop_first = {
-            let (_, rest) = b.split_first().unwrap();
-            sublist(a, rest)
-        };
-        let drop_last = {
-            let (_, rest) = b.split_last().unwrap();
-            sublist(a, rest)
-        };
-        match (drop_first, drop_last) {
-            // if either arm returns Equal or Sublist, it's a sublist.
-            // this is because there exists a path which reduces B to A.
-            // Therefore, the only case in which it's Unequal is if both
-            // recursive arms are unequal.
-            (Comparison::Unequal, Comparison::Unequal) => Comparison::Unequal,
-            (_, _) => Comparison::Sublist,
+        // OK, time to break out the Algorithms
+        if kmp_search(a, b).is_some() {
+            Comparison::Sublist
+        } else {
+            Comparison::Unequal
         }
     }
+}
+
+/// Knuth-Morris-Pratt substring search algorithm
+/// largely cribbed here from the pseuducode at
+/// https://en.wikipedia.org/wiki/Knuth%E2%80%93Morris%E2%80%93Pratt_algorithm#Description_of_pseudocode_for_the_search_algorithm
+#[allow(non_snake_case)]
+pub fn kmp_search<T: PartialEq>(W: &[T], S: &[T]) -> Option<usize> {
+    let mut m = 0;
+    let mut i: isize = 0;
+    let T = kmp_table(W);
+
+    while ((m + i) as usize) < S.len() {
+        if W[i as usize] == S[(m + i) as usize] {
+            if i == (W.len() - 1) as isize {
+                return Some(m as usize);
+            }
+            i += 1;
+        } else {
+            if T[i as usize] > -1 {
+                m += i - T[i as usize];
+                i = T[i as usize];
+            } else {
+                m += 1;
+                i = 0;
+            }
+        }
+    }
+    None
+}
+
+/// Knuth-Morris-Pratt substring table-builder
+/// https://en.wikipedia.org/wiki/Knuth%E2%80%93Morris%E2%80%93Pratt_algorithm#Description_of_pseudocode_for_the_table-building_algorithm
+#[allow(non_snake_case)]
+fn kmp_table<C: PartialEq>(W: &[C]) -> Vec<isize> {
+    let mut T: Vec<isize> = std::iter::repeat(0).take(W.len()).collect();
+    T[0] = -1;
+
+    let mut pos: usize = 2;
+    let mut cnd: usize = 0;
+
+    while pos < W.len() {
+        if W[pos-1] == W[cnd] {
+            T[pos] = cnd as isize + 1;
+            cnd += 1;
+            pos += 1;
+        } else if cnd > 0 {
+            cnd = T[cnd] as usize
+        } else {
+            T[pos] = 0;
+            pos += 1;
+        }
+    }
+
+    T
 }
