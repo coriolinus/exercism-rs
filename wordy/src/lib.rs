e2281a8580583530028d49eb9c5372b487051114
Peter Goodspeed-Niklaus <peter.r.goodspeedniklaus@gmail.com>
2016-07-13 14:02:18 +0200

    Finally get the trait bounds right for `Operation`

--- wordy/src/lib.rs
+++ wordy/src/lib.rs
@@ -1,3 +1,4 @@
+
 use std::ops;
 
 enum Operation {
@@ -5,24 +6,22 @@ enum Operation {
     Sub,
     Mul,
     Div,
-    Pow,
 }
 
 impl Operation {
     fn operate_on<T>(&self, left: T, right: T) -> T
-        where T: ops::Add + ops::Sub + ops::Mul + ops::Div +
-            From<<T as ops::Add>::Output> +
-            From<<T as ops::Sub>::Output> +
-            From<<T as ops::Mul>::Output> +
-            From<<T as ops::Div>::Output>
+        where T:
+            ops::Add<Output=T> +
+            ops::Sub<Output=T> +
+            ops::Mul<Output=T> +
+            ops::Div<Output=T>
     {
         use Operation::*;
         match *self {
-            Add => T::from(left + right),
-            Sub => T::from(left - right),
-            Mul => T::from(left * right),
-            Div => T::from(left / right),
-            Pow => unimplemented!(),
+            Add => left + right,
+            Sub => left - right,
+            Mul => left * right,
+            Div => left / right,
         }
     }
 }
@@ -32,12 +31,31 @@ pub struct WordProblem {
 }
 
 impl WordProblem {
+    // Not a fan of this style; I'd greatly prefer the constructor
+    // pub fn new(command: &str) -> Option<WordProblem>
+    //
+    // I get that it makes testing a little simpler to do it like this,
+    // but in real life, I want errors to show up as soon as possible,
+    // so that you can fix them immediately. This implies parsing
+    // problems like this in the constructor, so that the errors at least
+    // stay local. Otherwise, you have invalid WordProblems floating around
+    // within your code, just waiting for someone to call `.answer()`
+    // somewhere entirely nonlocal, before they strike.
     pub fn new(command: &str) -> WordProblem {
         WordProblem {command: command.to_string() }
     }
 
     pub fn answer(&self) -> Result<isize, &'static str> {
-        unimplemented!();
+        match WordProblem::parse(&self.command) {
+            Some((left, operation, right)) => {
+                Ok(operation.operate_on(left, right))
+            }
+            None => Err("Can't parse that command!"),
+        }
+    }
+
+    fn parse(command: &str) -> Option<(isize, Operation, isize)> {
+        unimplemented!()
     }
 
 }
