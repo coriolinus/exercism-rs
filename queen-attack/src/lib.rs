6155a9b7c36c595f20eff54b46f54a611b2688d2
Peter Goodspeed-Niklaus <peter.r.goodspeedniklaus@gmail.com>
2016-07-18 10:57:49 +0200

    Implement @hershi suggestions as they're good ideas

--- queen-attack/src/lib.rs
+++ queen-attack/src/lib.rs
@@ -1,8 +1,12 @@
+const CHESS_MAX: i8 = 8;
+const CHESS_MIN: i8 = 0;
+
 // Really, rank and file only need three bits apiece.
 // I'm really tempted to do some bit-shifting magic to store
 // them both within a single u8, but I think that for an
 // exercise like this, that optimization would be both
 // premature and pointless.
+#[derive(Copy, Clone)]
 pub struct ChessPosition {
     rank: u8,
     file: u8,
@@ -13,7 +17,7 @@ impl ChessPosition {
     // it'd make more sense and save code and expose errors at compile time to
     // require `rank` and `file` to be u8s.
     pub fn new(rank: i8, file: i8) -> Result<ChessPosition, &'static str> {
-        if rank >= 8 || file >= 8 || rank < 0 || file < 0 {
+        if rank >= CHESS_MAX || file >= CHESS_MAX || rank < CHESS_MIN || file < CHESS_MIN {
             Err("Invalid Position")
         } else {
             Ok(ChessPosition {
@@ -21,6 +25,14 @@ impl ChessPosition {
                 file: file as u8,
             })
         }
+    }
+}
+
+impl std::ops::Sub for ChessPosition {
+    type Output = (i8, i8);
+
+    fn sub(self, other: ChessPosition) -> Self::Output {
+        (self.rank as i8 - other.rank as i8, self.file as i8 - other.file as i8)
     }
 }
 
@@ -34,12 +46,10 @@ impl Queen {
     }
 
     pub fn can_attack(&self, other: &Queen) -> bool {
-        if self.position.rank == other.position.rank || self.position.file == other.position.file ||
-           (self.position.rank as i8 - other.position.rank as i8).abs() ==
-           (self.position.file as i8 - other.position.file as i8).abs() {
-            true
-        } else {
-            false
+        self.position.rank == other.position.rank || self.position.file == other.position.file ||
+        {
+            let (d_rank, d_file) = self.position - other.position;
+            d_rank.abs() == d_file.abs()
         }
     }
 }
