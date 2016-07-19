cf65bc88694d8a625ef1dbfda97a95a6ed335547
Peter Goodspeed-Niklaus <peter.r.goodspeedniklaus@gmail.com>
2016-07-19 15:03:04 +0200

    First test passes; implement to_vlq and to_bytes

--- variable-length-quantity/src/lib.rs
+++ variable-length-quantity/src/lib.rs
@@ -1,6 +1,20 @@
+pub fn to_vlq(mut value: u32) -> Vec<u8> {
+    let mut output = vec![(value & 0x7f) as u8];
+    value >>= 7;
+
+    while value != 0 {
+        output.push(0x80 | (value & 0x7f) as u8);
+        value >>= 7;
+    }
+
+    output.reverse();
+
+    output
+}
+
 /// Convert a list of numbers to a stream of bytes encoded with variable length encoding.
 pub fn to_bytes(values: &[u32]) -> Vec<u8> {
-    unimplemented!()
+    values.iter().flat_map(|v| to_vlq(*v)).collect()
 }
 
 /// Given a stream of bytes, extract all numbers which are encoded in there.
