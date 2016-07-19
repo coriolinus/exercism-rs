ace8cdfde1f74be406492b8a021b6d179785f277
Peter Goodspeed-Niklaus <peter.r.goodspeedniklaus@gmail.com>
2016-07-19 20:34:11 +0200

    All tests pass

--- nucleotide-codons/tests/codons.rs
+++ nucleotide-codons/tests/codons.rs
@@ -7,28 +7,25 @@ fn test_methionine() {
 }
 
 #[test]
-#[ignore]
 fn test_cysteine_tgt() {
     let info = codons::parse(make_pairs());
     assert_eq!(info.name_for("TGT"), Ok("cysteine"));
 }
 
 #[test]
-#[ignore]
-fn test_cysteine_tgy() { // "compressed" name for TGT and TGC
+fn test_cysteine_tgy() {
+    // "compressed" name for TGT and TGC
     let info = codons::parse(make_pairs());
     assert_eq!(info.name_for("TGY"), Ok("cysteine"));
 }
 
 #[test]
-#[ignore]
 fn test_stop() {
     let info = codons::parse(make_pairs());
     assert_eq!(info.name_for("TAA"), Ok("stop codon"));
 }
 
 #[test]
-#[ignore]
 fn test_valine() {
     let info = codons::parse(make_pairs());
     assert_eq!(info.name_for("GTN"), Ok("valine"));
@@ -36,14 +33,12 @@ fn test_valine() {
 
 
 #[test]
-#[ignore]
 fn test_isoleucine() {
     let info = codons::parse(make_pairs());
     assert_eq!(info.name_for("ATH"), Ok("isoleucine"));
 }
 
 #[test]
-#[ignore]
 fn test_arginine_name() {
     // In arginine CGA can be "compresed" both as CGN and as MGR
     let info = codons::parse(make_pairs());
@@ -53,7 +48,6 @@ fn test_arginine_name() {
 }
 
 #[test]
-#[ignore]
 fn test_invalid() {
     let info = codons::parse(make_pairs());
     assert!(info.name_for("").is_err());
@@ -64,34 +58,33 @@ fn test_invalid() {
 
 // The input data constructor. Returns a list of codon, name pairs.
 fn make_pairs() -> Vec<(&'static str, &'static str)> {
-    let grouped = vec![
-        ("isoleucine", vec!["ATT", "ATC", "ATA"]),
-        ("leucine", vec!["CTT", "CTC", "CTA", "CTG", "TTA", "TTG"]),
-        ("valine", vec!["GTT", "GTC", "GTA", "GTG"]),
-        ("phenylalanine", vec!["TTT", "TTC"]),
-        ("methionine", vec!["ATG"]),
-        ("cysteine", vec!["TGT", "TGC"]),
-        ("alanine", vec!["GCT", "GCC", "GCA", "GCG"]),
-        ("glycine", vec!["GGT", "GGC", "GGA", "GGG"]),
-        ("proline", vec!["CCT", "CCC", "CCA", "CCG"]),
-        ("threonine", vec!["ACT", "ACC", "ACA", "ACG"]),
-        ("serine", vec!["TCT", "TCC", "TCA", "TCG", "AGT", "AGC"]),
-        ("tyrosine", vec!["TAT", "TAC"]),
-        ("tryptophan", vec!["TGG"]),
-        ("glutamine", vec!["CAA", "CAG"]),
-        ("asparagine", vec!["AAT", "AAC"]),
-        ("histidine", vec!["CAT", "CAC"]),
-        ("glutamic acid", vec!["GAA", "GAG"]),
-        ("aspartic acid", vec!["GAT", "GAC"]),
-        ("lysine", vec!["AAA", "AAG"]),
-        ("arginine", vec!["CGT", "CGC", "CGA", "CGG", "AGA", "AGG"]),
-        ("stop codon", vec!["TAA", "TAG", "TGA"])];
+    let grouped = vec![("isoleucine", vec!["ATT", "ATC", "ATA"]),
+                       ("leucine", vec!["CTT", "CTC", "CTA", "CTG", "TTA", "TTG"]),
+                       ("valine", vec!["GTT", "GTC", "GTA", "GTG"]),
+                       ("phenylalanine", vec!["TTT", "TTC"]),
+                       ("methionine", vec!["ATG"]),
+                       ("cysteine", vec!["TGT", "TGC"]),
+                       ("alanine", vec!["GCT", "GCC", "GCA", "GCG"]),
+                       ("glycine", vec!["GGT", "GGC", "GGA", "GGG"]),
+                       ("proline", vec!["CCT", "CCC", "CCA", "CCG"]),
+                       ("threonine", vec!["ACT", "ACC", "ACA", "ACG"]),
+                       ("serine", vec!["TCT", "TCC", "TCA", "TCG", "AGT", "AGC"]),
+                       ("tyrosine", vec!["TAT", "TAC"]),
+                       ("tryptophan", vec!["TGG"]),
+                       ("glutamine", vec!["CAA", "CAG"]),
+                       ("asparagine", vec!["AAT", "AAC"]),
+                       ("histidine", vec!["CAT", "CAC"]),
+                       ("glutamic acid", vec!["GAA", "GAG"]),
+                       ("aspartic acid", vec!["GAT", "GAC"]),
+                       ("lysine", vec!["AAA", "AAG"]),
+                       ("arginine", vec!["CGT", "CGC", "CGA", "CGG", "AGA", "AGG"]),
+                       ("stop codon", vec!["TAA", "TAG", "TGA"])];
     let mut pairs = Vec::<(&'static str, &'static str)>::new();
     for (name, codons) in grouped.into_iter() {
         for codon in codons {
             pairs.push((codon, name));
         }
-    };
+    }
     pairs.sort_by(|&(_, a), &(_, b)| a.cmp(b));
-    return pairs
+    return pairs;
 }
