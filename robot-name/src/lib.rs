b9a493e7dd9d8acf63f9e21faa804ccc3d54e40b
Peter Goodspeed-Niklaus <peter.r.goodspeedniklaus@gmail.com>
2016-07-20 22:21:17 +0200

    Add experimental broken implementations for all required functions

--- robot-name/src/lib.rs
+++ robot-name/src/lib.rs
@@ -1,3 +1,6 @@
+extern crate rand;
+use rand::{thread_rng, sample};
+
 /*
 These are the expected signatures:
 
@@ -9,19 +12,23 @@ impl Robot {
 */
 
 pub struct Robot {
-    name: String,
+    name: Option<String>,
 }
 
 impl Robot {
     pub fn new() -> Robot {
-        unimplemented!()
+        Robot {name: None}
     }
 
-    pub fn name<'a>(&'a self) -> &'a str {
-        unimplemented!()
+    pub fn name<'a>(&'a mut self) -> &'a str {
+        if self.name.is_none() {
+            self.reset_name();
+        }
+        &self.name.unwrap()
     }
 
     pub fn reset_name(&mut self) {
-        
+        let mut rng = thread_rng();
+        self.name = sample(&mut rng, 'A'..'Z', 2).append(sample(&mut rng, '0'..'9', 3)).iter().collect()
     }
 }
