799ae320a5402678ef9803e38fcdea9d3f878d0a
Peter Goodspeed-Niklaus <peter.r.goodspeedniklaus@gmail.com>
2016-07-24 10:51:10 +0200

    First test passses

--- forth/src/lib.rs
+++ forth/src/lib.rs
@@ -1,7 +1,9 @@
 pub type Value = i32;
 pub type ForthResult = Result<(), Error>;
 
-pub struct Forth;
+pub struct Forth {
+    stack: Vec<i16>,
+}
 
 #[derive(Debug, PartialEq)]
 pub enum Error {
@@ -13,11 +15,14 @@ pub enum Error {
 
 impl Forth {
     pub fn new() -> Forth {
+        Forth { stack: Vec::new() }
     }
 
     pub fn format_stack(&self) -> String {
+        self.stack.iter().fold(String::new(), |string, num| string + &num.to_string())
     }
 
     pub fn eval(&mut self, input: &str) -> ForthResult {
+        unimplemented!()
     }
 }
