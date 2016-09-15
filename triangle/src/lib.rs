678e963baea53a39a2dc23d0fcc352a94a784cca
Peter Goodspeed-Niklaus <peter.r.goodspeedniklaus@gmail.com>
2016-09-15 12:20:40 +0200

    Implement all required functions

--- triangle/src/lib.rs
+++ triangle/src/lib.rs
@@ -1,31 +1,60 @@
+extern crate num_traits;
+use num_traits::Zero;
+
 #[derive(Debug)]
 pub enum TriangleError {
     ZeroLengthSide,
     ImpossibleSideLengths,
 }
 
-pub struct Triangle {
-    sides: [usize; 3],
+pub struct Triangle<T> {
+    sides: [T; 3],
 }
 
-impl Triangle {
-    pub fn build(sides: [usize; 3]) -> Result<Triangle, TriangleError> {
-        if sides.iter().any(|s| *s == 0) {
+impl<T> Triangle<T>
+    where T: Clone + PartialEq + Ord + Zero
+{
+    /// True if the longest side is shorter than the sum of the other two sides
+    pub fn is_possible_triangle(sides: &mut [T; 3]) -> bool {
+        sides.sort();
+        sides[0].clone() + sides[1].clone() >= *&sides[2]
+    }
+
+    pub fn build(mut sides: [T; 3]) -> Result<Triangle<T>, TriangleError> {
+        if sides.iter().any(|s| *s <= T::zero()) {
             Err(TriangleError::ZeroLengthSide)
+        } else if !Triangle::is_possible_triangle(&mut sides) {
+            Err(TriangleError::ImpossibleSideLengths)
         } else {
             Ok(Triangle { sides: sides })
         }
     }
 
+    /// Counts how many sides are of equal length
+    fn count_equal_sides(&self) -> u8 {
+        let mut equals = 0;
+
+        for (i1, i2) in vec![(0, 1), (1, 2), (2, 0)] {
+            if self.sides[i1] == self.sides[i2] {
+                equals += 1;
+            }
+        }
+
+        equals
+    }
+
+    /// True when two sides have the same length
     pub fn is_isosceles(&self) -> bool {
-        unimplemented!()
+        self.count_equal_sides() >= 2
     }
 
+    /// True when all three sides have unique lengths
     pub fn is_scalene(&self) -> bool {
-        unimplemented!()
+        self.count_equal_sides() == 0
     }
 
+    /// True when all three sides have the same length
     pub fn is_equilateral(&self) -> bool {
-        self.sides[0] == self.sides[1] && self.sides[1] == self.sides[2]
+        self.count_equal_sides() == 3
     }
 }
