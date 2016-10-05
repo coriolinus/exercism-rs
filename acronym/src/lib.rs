6beb006cd37cf09dbbb3349bfb545d63c58848b8
Peter Goodspeed-Niklaus <peter.r.goodspeedniklaus@gmail.com>
2016-10-05 17:51:46 +0200

    New problem: acronym

---
+++ acronym/src/lib.rs
@@ -0,0 +1,9 @@
+pub fn abbreviate(phrase: &str) -> String {
+    let mut acronym = String::new();
+
+    let mut initial = true;
+
+    phrase.chars()
+        .filter(|&c| unimplemented!())
+        .collect()
+}
