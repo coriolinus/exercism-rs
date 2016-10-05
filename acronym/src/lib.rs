a20a16c2d347be4de2bba1b100adae51fb1f06ae
Peter Goodspeed-Niklaus <peter.r.goodspeedniklaus@gmail.com>
2016-10-05 18:00:11 +0200

    Initial implementation; first test passes

--- acronym/src/lib.rs
+++ acronym/src/lib.rs
@@ -1,9 +1,23 @@
 pub fn abbreviate(phrase: &str) -> String {
-    let mut acronym = String::new();
-
+    // is this the first letter in a phrase / word?
     let mut initial = true;
 
-    phrase.chars()
-        .filter(|&c| unimplemented!())
+    phrase.trim()
+        .chars()
+        .filter(|&ch| {
+            if initial && ch.is_alphabetic() {
+                // initial characters should be included if alphabetic
+                initial = false;
+                true
+            } else if !ch.is_alphabetic() {
+                // the next character after a non-alphabetic one should be an initial
+                initial = true;
+                false
+            } else if ch.is_uppercase() {
+                true
+            } else {
+                false
+            }
+        })
         .collect()
 }
