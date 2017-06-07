38243f28b0f7e69455603a239cc3bb47c616dce3
Peter Goodspeed-Niklaus <peter.r.goodspeedniklaus@gmail.com>
2017-06-07 13:54:32 +0200

    Clean up the range syntax

--- grains/src/lib.rs
+++ grains/src/lib.rs
@@ -1,11 +1,8 @@
 pub fn square(s: u32) -> u64 {
-    if s == 0 {
-        panic!("Square must be between 1 and 64")
+    match s {
+        1...64 => 2u64.pow(s - 1),
+        _ => panic!("Square must be between 1 and 64"),
     }
-    if s > 64 {
-        panic!("Square must be between 1 and 64")
-    }
-    2u64.pow(s - 1)
 }
 
 pub fn total() -> u64 {
