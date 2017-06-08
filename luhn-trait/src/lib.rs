18da54edc5af6b8dcad9f7ac4d96e911ba73f26b
Peter Goodspeed-Niklaus <peter.r.goodspeedniklaus@gmail.com>
2017-06-08 15:23:21 +0200

    Replace blanket impl with default impl

--- luhn-trait/src/lib.rs
+++ luhn-trait/src/lib.rs
@@ -2,11 +2,9 @@ extern crate luhn;
 use luhn::is_valid;
 
 pub trait Luhn: ToString {
-    fn valid_luhn(&self) -> bool;
-}
-
-impl<T: ToString> Luhn for T {
     fn valid_luhn(&self) -> bool {
         is_valid(&self.to_string())
     }
 }
+
+impl<T: ToString> Luhn for T {}
