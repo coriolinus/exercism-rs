c797705baf07d94e293c9bd3c8550cc29e8f883a
Peter Goodspeed-Niklaus <peter.r.goodspeedniklaus@gmail.com>
2017-06-12 18:37:10 +0200

    Mysterious changes that I don't know anything about
    These were probably unfinished work at one point, but now are
    nothing but a signal to revert to the previous commit for
    affected files.

--- react/src/lib.rs
+++ react/src/lib.rs
@@ -1,13 +1,15 @@
-use std::collections::{HashMap, HashSet, VecDeque};
+use std::collections::HashSet;
+
+mod merge;
+use merge::merge;
 
 // Because these are passed without & to some functions,
 // it will probably be necessary for these two types to be Copy.
 pub type CellID = usize;
 pub type CallbackID = usize;
 
-pub struct Reactor<'a, T> {
+pub struct Reactor<'a, T: Copy + PartialEq> {
     cells: Vec<Cell<'a, T>>,
-    forward_references: HashMap<CellID, VecDeque<CellID>>,
     callbacks: Vec<Box<FnMut(T) + 'a>>,
 }
 
@@ -23,15 +25,44 @@ pub enum ReactorError {
 enum CellType<'a, T> {
     Input,
     Compute {
-        dependencies: Vec<CellID>,
         update: Box<Fn(&[T]) -> T + 'a>,
-        callbacks: HashSet<CallbackID>,
+        dependency_ids: Vec<CellID>,
+        callback_ids: HashSet<CallbackID>,
     },
 }
 
-struct Cell<'a, T> {
+struct Cell<'a, T: Copy + PartialEq> {
     value: T,
     c_type: CellType<'a, T>,
+    fwd_references: Vec<CellID>,
+    dirty: bool,
+}
+
+impl<'a, T: Copy + PartialEq> Cell<'a, T> {
+    fn create_input(initial: T) -> Cell<'a, T> {
+        Cell {
+            value: initial,
+            c_type: CellType::Input,
+            fwd_references: Vec::new(),
+            dirty: false,
+        }
+    }
+
+    fn create_compute<F: Fn(&[T]) -> T + 'a>(initial: T,
+                                             dependency_ids: &[CellID],
+                                             update: F)
+                                             -> Cell<'a, T> {
+        Cell {
+            value: initial,
+            c_type: CellType::Compute {
+                update: Box::new(update),
+                dependency_ids: Vec::from(dependency_ids),
+                callback_ids: HashSet::new(),
+            },
+            fwd_references: Vec::new(),
+            dirty: false,
+        }
+    }
 }
 
 // You are guaranteed that Reactor will only be tested against types that are Copy + PartialEq.
@@ -39,18 +70,8 @@ impl<'a, T: Copy + PartialEq> Reactor<'a, T> {
     pub fn new() -> Self {
         Reactor {
             cells: Vec::new(),
-            forward_references: HashMap::new(),
             callbacks: Vec::new(),
         }
-    }
-
-    /// Creates an input cell with the specified initial value, returning its ID.
-    pub fn create_input(&mut self, initial: T) -> CellID {
-        self.cells.push(Cell {
-            value: initial,
-            c_type: CellType::Input,
-        });
-        self.cells.len() - 1
     }
 
     /// get the current value of each of the specified dependencies
@@ -59,6 +80,33 @@ impl<'a, T: Copy + PartialEq> Reactor<'a, T> {
             .map(|d_id| self.value(*d_id))
             .collect::<Option<Vec<_>>>()
             .ok_or(ReactorError::DependencyDoesntExist)
+    }
+
+    /// Trace all cells which have become dirty as a result of this operation and mark them.
+    fn mark(&mut self, cell: CellID) -> Result<(), ReactorError> {
+        if cell < self.cells.len() {
+            self.cells[cell].dirty = true;
+
+            // we need to clone the list of forward references to ensure we don't
+            // change the list as we're iterating over it; it's an issue of needing
+            // to mutably borrow what would otherwise already be immutably borrowed.
+            for downstream in self.cells[cell].fwd_references.clone() {
+                try!(self.mark(downstream));
+            }
+            Ok(())
+        } else {
+            Err(ReactorError::CellDoesntExist(cell))
+        }
+    }
+
+    /// Update all marked cells in sequence.
+    ///
+    /// This is a convenience function for update_
+    // fn update(&mut self, cell: CellID)
+    /// Creates an input cell with the specified initial value, returning its ID.
+    pub fn create_input(&mut self, initial: T) -> CellID {
+        self.cells.push(Cell::create_input(initial));
+        self.cells.len() - 1
     }
 
     /// Creates a compute cell with the specified dependencies and compute function.
@@ -77,27 +125,13 @@ impl<'a, T: Copy + PartialEq> Reactor<'a, T> {
                                                  compute_func: F)
                                                  -> Result<CellID, ReactorError> {
 
-        // Collect the current values of all dependencies.
-        // We'll use this in a second to initialize the cache.
-        let d_values = try!(self.get_d_values(dependencies));
-
-        // Cell id
         let cid = self.cells.len();
+        let initial = compute_func(&try!(self.get_d_values(dependencies)));
+        self.cells.push(Cell::create_compute(initial, dependencies, compute_func));
 
-        // insert the cell into the cells vec
-        self.cells.push(Cell {
-            value: compute_func(&d_values),
-            c_type: CellType::Compute {
-                dependencies: Vec::from(dependencies),
-                update: Box::new(compute_func),
-                callbacks: HashSet::new(),
-            },
-        });
-
-        // Make a separate list of forward dependencies, to simplify the process
-        // of tracing updates forward from a set_value call.
+        // Update the forward dependencies for all depended-on cells
         for dependency in dependencies {
-            self.forward_references.entry(*dependency).or_insert(VecDeque::new()).push_back(cid);
+            self.cells[*dependency].fwd_references.push(cid);
         }
 
         Ok(cid)
@@ -118,54 +152,6 @@ impl<'a, T: Copy + PartialEq> Reactor<'a, T> {
         }
     }
 
-    /// Update a particular cell by id.
-    ///
-    /// If the cell is valid, return a bool indicating whether the cell changed
-    /// as a result of this update.
-    ///
-    /// Otherwise, return a ReactorError.
-    fn update_compute_cache(&mut self, id: CellID) -> Result<bool, ReactorError> {
-        if id >= self.cells.len() {
-            Err(ReactorError::CellDoesntExist(id))
-        } else {
-            // borrow self immutably a few times here to get a new cache value
-            // Ideally, we'd be able to also pull a mutable reference to the
-            // cell's callbacks out of the same match; if we could, we'd avoid
-            // a second match later on. However, that would mean borrowing self
-            // mutably, and that can happen only once at a time. It wouldn't work
-            // here, because we need at least two immutable borrows of self:
-            // one to get the references to the cell's contents, and another
-            // to call self.value(). We just have to match the cell type again,
-            // later, to get a mutable reference to the cell's callbacks which we
-            // can use to actually call them.
-            let new_cache = try!(match self.cells[id].c_type {
-                CellType::Input => Err(ReactorError::NotAComputeCell(id)),
-                CellType::Compute { ref dependencies, ref update, .. } => {
-                    Ok(update(&try!(self.get_d_values(dependencies))))
-                }
-            });
-
-            let changed = self.cells[id].value != new_cache;
-            self.cells[id].value = new_cache;
-
-            if changed {
-                // we have to borrow self mutably to get access to cell_callbacks
-                for cbid in try!(match self.cells[id].c_type {
-                        CellType::Input => Err(ReactorError::NotAComputeCell(id)),
-                        CellType::Compute { ref mut callbacks, .. } => Ok(callbacks),
-                    })
-                    .iter() {
-                    // see http://stackoverflow.com/a/39532428/504550 for
-                    // a small discussion of why this syntax is necessary.
-                    // In short, the FnMut binding must itself be mutable,
-                    // and the compiler isn't smart enough to figure that
-                    // out itself from simply `self.callbacks[*cbid](new_cache);`
-                    (*&mut self.callbacks[*cbid])(new_cache);
-                }
-            }
-            Ok(changed)
-        }
-    }
 
     /// Sets the value of the specified input cell.
     ///
@@ -186,36 +172,9 @@ impl<'a, T: Copy + PartialEq> Reactor<'a, T> {
                     self.cells[id].value = new_value;
 
                     // go through and update everything which depended on this cell now
-                    let mut cells_to_update = Vec::new();
-                    if let Some(fwd_ref) = self.forward_references.get(&id) {
-                        let mut fwd_ref = fwd_ref.clone();
-
-                        while fwd_ref.len() > 0 {
-                            let updating_cell_id = fwd_ref.pop_front().unwrap();
-                            // Ideally, instead of pushing to a list of cells to update here,
-                            // we'd just update the compute cache. Unfortunately, *self has
-                            // been borrowed mutably once already, and we can't do it again.
-                            //
-                            // The implication is that we can't filter, here, only those
-                            // cells whose values actually changed from the computation;
-                            // we have to propagate through all updated cells even if some
-                            // of them don't change.
-                            cells_to_update.push(updating_cell_id);
-                            if let Some(n_fwd_ref) = self.forward_references
-                                .get(&updating_cell_id) {
-                                fwd_ref.extend(n_fwd_ref);
-                            }
-                        }
-                    }
-                    // we depend on the fact that no cell will ever depend on
-                    // a cell with a higher ID in order to ensure that no callback is called
-                    // more than once.
-                    cells_to_update.sort();
-                    cells_to_update.dedup();
-
-                    for cell in cells_to_update {
-                        try!(self.update_compute_cache(cell));
-                    }
+                    self.mark(id);
+                    // then update the marked ones in order
+                    unimplemented!();
 
                     Ok(())
                 }
@@ -250,8 +209,8 @@ impl<'a, T: Copy + PartialEq> Reactor<'a, T> {
 
         match self.cells[id].c_type {
             CellType::Input => Err(ReactorError::NotAComputeCell(id)),
-            CellType::Compute { ref mut callbacks, .. } => {
-                callbacks.insert(cbid);
+            CellType::Compute { ref mut callback_ids, .. } => {
+                callback_ids.insert(cbid);
                 Ok(cbid)
             }
         }
@@ -276,8 +235,8 @@ impl<'a, T: Copy + PartialEq> Reactor<'a, T> {
 
         match self.cells[cell].c_type {
             CellType::Input => Err(ReactorError::NotAComputeCell(cell)),
-            CellType::Compute { ref mut callbacks, .. } => {
-                if callbacks.remove(&callback) {
+            CellType::Compute { ref mut callback_ids, .. } => {
+                if callback_ids.remove(&callback) {
                     // callback did exist in the callback set
                     Ok(())
                 } else {
