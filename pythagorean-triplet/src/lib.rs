4104e9a4032706bbaf95e91555b6e3ce2bc183b8
Peter Goodspeed-Niklaus <peter.r.goodspeedniklaus@gmail.com>
2017-08-12 16:08:48 +0200

    Solve the exercise.
    Instead of trying to get fancy with math, it turns out to be more reliable
    (and still quite fast) to just brute-force it by iterating over all possible
    triples which sum to the desired number.

--- pythagorean-triplet/src/lib.rs
+++ pythagorean-triplet/src/lib.rs
@@ -6,87 +6,67 @@ extern crate try_opt;
 /// WTF is up with this interface, though? This is the opposite of modular!
 pub fn find() -> Option<u64> {
     let (a, b, c) = try_opt!(find_triplet_with_sum(1000));
-    Some(a + b + c)
+    Some(a * b * c)
+}
+
+pub type Triplet = (u64, u64, u64);
+
+fn is_pythagorean(t: Triplet) -> bool {
+    t.0 * t.0 + t.1 * t.1 == t.2 * t.2
 }
 
 /// Find a Pythagorean Triplet with a given sum
 ///
 /// This is more what a function's interface should look like.
-///
-/// Anyway, per Wikipedia: "Together, the Stifel and Ozanam sequences produce all primitive
-/// triples of the Plato and Pythagoras families respectively". This does leave out the
-/// Fermat family, and perhaps others (?); my pure math knowledge is insufficient on this
-/// point. However, this gives us a decent chance to generate the correct number.
-pub fn find_triplet_with_sum(sum: u64) -> Option<(u64, u64, u64)> {
-    find_triplet_with_sum_using(sum, stifel).or(find_triplet_with_sum_using(sum, ozanam))
+pub fn find_triplet_with_sum(sum: u64) -> Option<Triplet> {
+    Triplets::new(sum).filter(|&t| is_pythagorean(t)).next()
 }
 
-fn find_triplet_with_sum_using<F>(sum: u64, method: F) -> Option<(u64, u64, u64)>
-where
-    F: Fn(u64) -> (u64, u64, u64),
-{
-    let mut i = 0;
-    loop {
-        i += 1;
-        let (a, b, c) = method(i);
-        let method_sum = a + b + c;
+/// Iterator over possible natural triplets which sum to the desired value
+///
+/// `c` is always equal to `total - (a + b)`.
+/// The property `b >= a` is always observed.
+/// The property `c > b` is always observed.
+struct Triplets {
+    total: u64,
+    a: u64,
+    b: u64,
+}
 
-        if method_sum == sum {
-            return Some((a, b, c));
+impl Triplets {
+    fn new(sum: u64) -> Triplets {
+        Triplets {
+            total: sum,
+            a: 1,
+            b: 0,
         }
-        if method_sum > sum {
-            return None;
-        }
+    }
+
+    fn obeys_properties(&self) -> bool {
+        self.a <= self.b && self.a + self.b < self.total && self.b < self.c()
+    }
+
+    // be careful when using this: if you don't already know that a and b are valid,
+    // it's possible to get an underflow
+    fn c(&self) -> u64 {
+        self.total - (self.a + self.b)
     }
 }
 
-/// Generate a number from the Stifel sequence
-///
-/// See https://en.wikipedia.org/wiki/Formulas_for_generating_Pythagorean_triples
-fn stifel(n: u64) -> (u64, u64, u64) {
-    let denominator = (2 * n) + 1;
-    let numerator = (n * denominator) + n;
-    let hypotenuse = numerator + 1;
-    (denominator, numerator, hypotenuse)
-}
-
-#[cfg(test)]
-pub fn t_stifel(n: u64) -> (u64, u64, u64) {
-    stifel(n)
-}
-
-/// Generate a number from the Ozanam sequence
-///
-/// See https://en.wikipedia.org/wiki/Formulas_for_generating_Pythagorean_triples
-fn ozanam(n: u64) -> (u64, u64, u64) {
-    let denominator = (4 * n) + 4;
-    let numerator = (4 * n) + 3 + (denominator * n);
-    let hypotenuse = numerator + 2;
-    (denominator, numerator, hypotenuse)
-}
-
-#[cfg(test)]
-pub fn t_ozanam(n: u64) -> (u64, u64, u64) {
-    ozanam(n)
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test_stifel() {
-        assert_eq!(t_stifel(1), (3, 4, 5));
-        assert_eq!(t_stifel(2), (5, 12, 13));
-        assert_eq!(t_stifel(3), (7, 24, 25));
-        assert_eq!(t_stifel(4), (9, 40, 41));
-    }
-
-    #[test]
-    fn test_ozanam() {
-        assert_eq!(t_ozanam(1), (8, 15, 17));
-        assert_eq!(t_ozanam(2), (12, 35, 37));
-        assert_eq!(t_ozanam(3), (16, 63, 65));
-        assert_eq!(t_ozanam(4), (20, 99, 101));
+impl Iterator for Triplets {
+    type Item = Triplet;
+    fn next(&mut self) -> Option<Triplet> {
+        // can we increment b?
+        self.b += 1;
+        if self.obeys_properties() {
+            return Some((self.a, self.b, self.c()));
+        }
+        // if not, can we increment a?
+        self.a += 1;
+        self.b = self.a;
+        if self.obeys_properties() {
+            return Some((self.a, self.b, self.c()));
+        }
+        None
     }
 }
