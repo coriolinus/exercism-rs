73ac3ae2f8f5810def50d9a85d953cb5e0bec8f8
Peter Goodspeed-Niklaus <peter.r.goodspeedniklaus@gmail.com>
2017-11-09 14:02:44 +0100

    Minor optimization: pre-allocate destination

--- proverb/src/lib.rs
+++ proverb/src/lib.rs
@@ -1,6 +1,6 @@
 
 pub fn build_proverb(items: Vec<&str>) -> String {
-    let mut stanzas = Vec::new();
+    let mut stanzas = Vec::with_capacity(items.len());
     for index in 0..items.len() {
         if index == items.len() - 1 {
             stanzas.push(format!("And all for the want of a {}.", items[0]));
