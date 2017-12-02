e12bfc2751ceede15419ade1b5bbeefe0a29c76e
Peter Goodspeed-Niklaus <peter.r.goodspeedniklaus@gmail.com>
2017-12-02 15:18:40 +0100

    add, complete excercise macro

---
+++ macros/src/lib.rs
@@ -0,0 +1,14 @@
+#[macro_export]
+macro_rules! hashmap {
+    () => {::std::collections::HashMap::new()};
+    ($($k:expr => $v:expr),*) => {hashmap!($($k => $v,)*)};
+    ($($k:expr => $v:expr,)*) => {
+        {
+            let mut hm = ::std::collections::HashMap::new();
+            $(
+                hm.insert($k, $v);
+            )*
+            hm
+        }
+    };
+}
