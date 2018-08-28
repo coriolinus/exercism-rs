89220f738d6f91930291c9d31d9d2af77caeb0d5
Peter Goodspeed-Niklaus <peter.r.goodspeedniklaus@gmail.com>
2018-08-28 11:56:09 +0200

    Partial work computing the pythagorean triplet at compile time
    Unfortunately, we're blocked here for now: const functions cannot
    yet contain if statements, and we need that to perform the recursive
    search for a valid triplet. It's a race, now: will rust's const fn
    support evolve faster than the pythagorean triplet test suite, or
    not? There are open issues for both.
    
    I'll likely just come back and try this again in a few months; maybe
    we'll have a resolution then.

--- pythagorean-triplet/src/lib.rs
+++ pythagorean-triplet/src/lib.rs
@@ -1,72 +1,51 @@
-#[macro_use]
-extern crate try_opt;
+// this requires a nightly compiler, but the result is that we can compute
+// the answer at compile-time, which is pretty cool
+#![feature(const_fn)]
+
+type N = u64;
+
+const TARGET: N = 1000;
+
+struct Triplet(N, N, N);
+
+impl Triplet {
+    const fn new(a: N, b: N) -> Triplet {
+        Triplet(a, b, TARGET - (a + b))
+    }
+
+    const fn product(&self) -> N {
+        self.0 * self.1 * self.2
+    }
+
+    const fn is_pythagorean(&self) -> bool {
+        (self.0 * self.0) + (self.1 * self.1) == (self.2 * self.2)
+    }
+
+    const fn first() -> Triplet {
+        Triplet(0, 0, TARGET)
+    }
+
+    const fn next(&self) -> Triplet {
+        if self.1 >= TARGET {
+            panic!("Couldn't find appropriate next triplet")
+        }
+        if self.0 + self.1 >= TARGET {
+            Triplet::new(0, self.1 + 1)
+        } else {
+            Triplet::new(self.0 + 1, self.1)
+        }
+    }
+}
+
+pub const fn const_find() -> N {
+    0
+}
+
+const ANSWER: N = const_find();
 
 /// Find the answer asked for by README.md
 ///
 /// WTF is up with this interface, though? This is the opposite of modular!
-pub fn find() -> Option<u64> {
-    let (a, b, c) = try_opt!(find_triplet_with_sum(1000));
-    Some(a * b * c)
-}
-
-pub type Triplet = (u64, u64, u64);
-
-fn is_pythagorean(t: Triplet) -> bool {
-    t.0 * t.0 + t.1 * t.1 == t.2 * t.2
-}
-
-/// Find a Pythagorean Triplet with a given sum
-///
-/// This is more what a function's interface should look like.
-pub fn find_triplet_with_sum(sum: u64) -> Option<Triplet> {
-    Triplets::new(sum).filter(|&t| is_pythagorean(t)).next()
-}
-
-/// Iterator over possible natural triplets which sum to the desired value
-///
-/// `c` is always equal to `total - (a + b)`.
-/// The property `b >= a` is always observed.
-/// The property `c > b` is always observed.
-struct Triplets {
-    total: u64,
-    a: u64,
-    b: u64,
-}
-
-impl Triplets {
-    fn new(sum: u64) -> Triplets {
-        Triplets {
-            total: sum,
-            a: 1,
-            b: 0,
-        }
-    }
-
-    fn obeys_properties(&self) -> bool {
-        self.a <= self.b && self.a + self.b < self.total && self.b < self.c()
-    }
-
-    // be careful when using this: if you don't already know that a and b are valid,
-    // it's possible to get an underflow
-    fn c(&self) -> u64 {
-        self.total - (self.a + self.b)
-    }
-}
-
-impl Iterator for Triplets {
-    type Item = Triplet;
-    fn next(&mut self) -> Option<Triplet> {
-        // can we increment b?
-        self.b += 1;
-        if self.obeys_properties() {
-            return Some((self.a, self.b, self.c()));
-        }
-        // if not, can we increment a?
-        self.a += 1;
-        self.b = self.a;
-        if self.obeys_properties() {
-            return Some((self.a, self.b, self.c()));
-        }
-        None
-    }
+pub fn find() -> Option<N> {
+    Some(ANSWER)
 }
