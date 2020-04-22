cf7a2a823547eda00cdb6df4b5ba16f2a2532763
Peter Goodspeed-Niklaus <peter.r.goodspeedniklaus@gmail.com>
2020-04-22 14:07:14 +0200

    Update atbash-cipher exercise
    Update to 2018 edition, rewrite solution with a single allocation.

--- atbash-cipher/src/lib.rs
+++ atbash-cipher/src/lib.rs
@@ -1,35 +1,77 @@
-use std::ascii::AsciiExt;
+mod separate {
+    use itertools::Itertools;
+    use std::iter::FromIterator;
 
-pub fn atbash(ch: char) -> char {
-    let tch = ch.to_ascii_lowercase();
-    if tch >= 'a' && tch <= 'z' {
-        ('z' as u8 - (tch as u8 - 'a' as u8)) as char
-    } else {
-        ch
+    pub trait Separate<'a, I, T, O>
+    where
+        I: IntoIterator<Item = T>,
+        T: Copy,
+        O: FromIterator<T>,
+    {
+        /// Separate a stream into groups, inserting a copy of T between each.
+        /// Then collect it into a String.
+        ///
+        /// This is a fused iterator.
+        fn separate(self, group_sep: T, group_size: usize) -> O;
+    }
+
+    impl<'a, I, T, O> Separate<'a, I, T, O> for I
+    where
+        I: 'a + IntoIterator<Item = T>,
+        <I as IntoIterator>::IntoIter: 'a,
+        T: 'a + Copy + PartialEq,
+        O: FromIterator<T>,
+    {
+        fn separate(self, group_sep: T, group_size: usize) -> O {
+            self.into_iter()
+                .fuse()
+                .chunks(group_size)
+                .into_iter()
+                .map(|chunk| {
+                    let d: Box<dyn Iterator<Item = T>> = Box::new(chunk);
+                    d
+                })
+                .interleave_shortest(std::iter::repeat(std::iter::once(group_sep)).map(|cyc| {
+                    let d: Box<dyn Iterator<Item = T>> = Box::new(cyc);
+                    d
+                }))
+                .flatten()
+                .with_position()
+                .filter_map(move |pos| {
+                    use itertools::Position::*;
+                    match pos {
+                        Only(c) => Some(c),
+                        First(c) => Some(c),
+                        Middle(c) => Some(c),
+                        Last(c) if c != group_sep => Some(c),
+                        _ => None,
+                    }
+                })
+                .collect()
+        }
     }
 }
 
-fn is_allowed_char(ch: &char) -> bool {
-    ch.is_alphanumeric() && ch.is_ascii()
+use separate::Separate;
+
+fn transpose(c: char) -> Option<char> {
+    if !c.is_ascii_alphanumeric() {
+        return None;
+    }
+    Some(if c.is_ascii_alphabetic() {
+        let c = (c as u8) | (1 << 5); // lowercase ascii value
+        (b'z' - (c - b'a')) as char
+    } else {
+        c
+    })
 }
 
-pub fn encode(s: &str) -> String {
-    // Not a huge fan of all the repetitive
-    // .collect() calls, or the .cloned in the .map,
-    // but that's the only way it'll compile, unfortunately.
-    s.chars()
-        .filter(is_allowed_char)
-        .map(atbash)
-        .collect::<Vec<_>>()
-        .chunks(5)
-        .map(|chunk| chunk.iter().cloned().collect::<String>())
-        .collect::<Vec<String>>()
-        .join(" ")
+/// "Encipher" with the Atbash cipher.
+pub fn encode(plain: &str) -> String {
+    plain.chars().flat_map(transpose).separate(' ', 5)
 }
 
-pub fn decode(s: &str) -> String {
-    s.chars()
-        .filter(is_allowed_char)
-        .map(atbash)
-        .collect()
+/// "Decipher" with the Atbash cipher.
+pub fn decode(cipher: &str) -> String {
+    cipher.chars().flat_map(transpose).collect()
 }
